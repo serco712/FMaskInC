831d372d321a0e8939c8f19930cf509146e54b40
serco712 <98959255+serco712@users.noreply.github.com>
2023-10-21 20:06:45 +0200

    DetectWater

---
+++ autoFmask.h
@@ -0,0 +1,29 @@
+/*
+ * Sergio Colet García
+ *
+ * AutoFmask.h
+ *
+ *
+ */
+
+#ifndef AUTOFMASK_H
+#define AUTOFMASK_H
+
+/* Include files */
+#include "types.h"
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include "ObservMask.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
